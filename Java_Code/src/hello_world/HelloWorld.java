eba3d0346f0ff39abaa6ae829acc2378c4e04afd
Miroslav <miroslav.l.metodiev@gmail.com>
2014-07-22 22:12:27 +0300

    Hello World Commit

--- Java_Code/src/hello_world/HelloWorld.java
+++ Java_Code/src/hello_world/HelloWorld.java
@@ -3,8 +3,13 @@ package hello_world;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	  System.out.println("Hello World");
+	  
+	  
+		
+	
+	  
 	}
 
 }
