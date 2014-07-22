1d13ba5c6c780fc84fee8525013e8b8413b1fa5a
Miroslav <miroslav.l.metodiev@gmail.com>
2014-07-22 22:18:47 +0300

    min and max types

--- Java_Code/src/hello_world/HelloWorld.java
+++ Java_Code/src/hello_world/HelloWorld.java
@@ -6,10 +6,14 @@ public class HelloWorld {
 		
 	  System.out.println("Hello World");
 	  
+	  byte bigByte = 127;
+	  short bigSHort = 32767;
+	  int bigInt = 21000000;
+	  long bigLong = 920000000;
+	  float bigFloat = 3.14F;
+	  double bigDouble = 3.1455954545454545;
 	  
-		
-	
-	  
+	  System.out.println(bigDouble);
 	}
 
 }
